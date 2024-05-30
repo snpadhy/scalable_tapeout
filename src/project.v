e07568b267d78a8707c1721addaecc5da8dce4b6
snpadhy <snpadhy@cutm.ac.in>
2024-05-30 16:32:53 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -22,7 +22,8 @@ module  tt_um_example( ena,clk,li, ri, ki, lio, rio );
     wire [47:0] lio;
     wire [47:0] rio;
 
-   
+   assign nli = ena ? li : 48'b0;  // If ena is high, pass li, otherwise 48'b0
+   assign nri = ena ? ri : 48'b0;  // If ena is high, pass ri, otherwise 48'b0
     sea_en x0(.clk(clk), .li(li), .ri(ri), .ki(ki), .nli(nli), .nri(nri));
    
     sea_de x1(.clk(clk), .nli(nli), .nri(nri), .ki(ki), .li(lio), .ri(rio));
